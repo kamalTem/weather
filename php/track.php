6ee928e07db0b759841d2b3fcb1ca7a32c59ab3b
kamalTem <40537019+kamalTem@users.noreply.github.com>
2018-09-29 17:07:36 +0400

    Add files via upload

---
+++ php/track.php
@@ -0,0 +1,20 @@
+<?php
+
+fimport requests(){
+  
+}
+s_city = "Petersburg,RU"
+city_id = 0
+appid = "758eb996d86d0e5e92ead4f71cf3764c"
+try:
+    res = requests.get("http://api.openweathermap.org/data/2.5/find",
+                 params={'q': s_city, 'type': 'like', 'units': 'metric', 'APPID': appid})
+    data = res.json()
+    cities = ["{} ({})".format(d['name'], d['sys']['country'])
+              for d in data['list']]
+    print("city:", cities)
+    city_id = data['list'][0]['id']
+    print('city_id=', city_id)
+except Exception as e:
+    print("Exception (find):", e)
+    pass
